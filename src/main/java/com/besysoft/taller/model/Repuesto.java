d39721fe6079af92319ccb85646ef3ad94c28df1
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 18:46:41 -0300

    Create Repuesto.java

---
+++ src/main/java/com/besysoft/taller/model/Repuesto.java
@@ -0,0 +1,11 @@
+package com.besysoft.taller.model;
+
+import java.io.Serializable;
+
+public class Repuesto implements Serializable {
+    private Long id;
+    private String marca;
+    private String modelo;
+    private String repuesto;
+    private double valor;
+}
