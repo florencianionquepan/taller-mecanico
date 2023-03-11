a7190cfb75fffbb9dcc04651d9a397a1e1f111e2
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 18:46:43 -0300

    Create Vehiculo.java

---
+++ src/main/java/com/besysoft/taller/model/Vehiculo.java
@@ -0,0 +1,13 @@
+package com.besysoft.taller.model;
+
+import java.io.Serializable;
+
+public class Vehiculo implements Serializable {
+    private Long id;
+    private String patente;
+    private int anio;
+    private String color;
+    private String marca;
+    private String modelo;
+
+}
