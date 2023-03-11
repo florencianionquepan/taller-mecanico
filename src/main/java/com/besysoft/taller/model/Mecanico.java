4c8afb44b8d859979c2902d12944e9817fd1ccf0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 20:32:37 -0300

    Create Mecanico.java

---
+++ src/main/java/com/besysoft/taller/model/Mecanico.java
@@ -0,0 +1,34 @@
+package com.besysoft.taller.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="mecanicos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mecanico implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombres;
+    private String apellido;
+    @Column(length = 15)
+    private String celular;
+    private String localidad;
+    private String codigoPostal;
+    private String calle;
+    private String numero;
+    private String departamento;
+    private String piso;
+
+    private boolean activo;
+    private String especialidad;
+}
