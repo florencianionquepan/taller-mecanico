51dea48012bc17af2eb161db5548e5a4369ef776
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 19:11:36 -0300

    Create Recepcionista.java

---
+++ src/main/java/com/besysoft/taller/model/Recepcionista.java
@@ -0,0 +1,12 @@
+package com.besysoft.taller.model;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+import java.io.Serializable;
+
+@Entity
+@DiscriminatorValue(value="Recep")
+public class Recepcionista extends Empleado implements Serializable {
+    //private OrdenTrabajo ordenTrabajo;
+}
