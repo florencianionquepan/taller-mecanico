5dc5d3c4abdfb0b1ef9de2ce84139eae3b7087ff
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 20:51:00 -0300

    Create ManoObra.java

---
+++ src/main/java/com/besysoft/taller/model/ManoObra.java
@@ -0,0 +1,22 @@
+package com.besysoft.taller.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.sql.Time;
+
+@Entity
+public class ManoObra implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String detalle;
+    private Time duracionHs;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private Mecanico mecanico;
+
+    @OneToOne(cascade = CascadeType.MERGE)
+    private OrdenTrabajo ordenTrabajo;
+
+}
