e70217c71414241b7115856f0c4223fb6e9098e1
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 20:34:12 -0300

    Update Repuesto.java

--- src/main/java/com/besysoft/taller/model/Repuesto.java
+++ src/main/java/com/besysoft/taller/model/Repuesto.java
@@ -1,11 +1,27 @@
 package com.besysoft.taller.model;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name="repuestos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Repuesto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String marca;
     private String modelo;
     private String repuesto;
+    @Column(nullable = false)
     private double valor;
 }
