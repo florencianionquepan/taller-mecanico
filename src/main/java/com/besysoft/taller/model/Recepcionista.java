b4e7f7dfabdd304a4c031e0aecdda1ac1659cf91
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 19:30:20 -0300

    Update Recepcionista.java

--- src/main/java/com/besysoft/taller/model/Recepcionista.java
+++ src/main/java/com/besysoft/taller/model/Recepcionista.java
@@ -8,5 +8,5 @@ import java.io.Serializable;
 @Entity
 @DiscriminatorValue(value="Recep")
 public class Recepcionista extends Empleado implements Serializable {
-    //private OrdenTrabajo ordenTrabajo;
+
 }
