182600f5e6682af071bf8413f4349c1bedc3e648
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 20:34:01 -0300

    Create Cliente.java

---
+++ src/main/java/com/besysoft/taller/model/Cliente.java
@@ -0,0 +1,41 @@
+package com.besysoft.taller.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name="clientes")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String nombres;
+    @Column(length = 80, nullable = false)
+    private String apellido;
+    @Column(length = 15)
+    private String celular;
+    private String localidad;
+    private String codigoPostal;
+    private String calle;
+    private String numero;
+    private String departamento;
+    private String piso;
+
+    @Column(length = 15)
+    private String telefonoLinea;
+
+    @Column(nullable = false,unique = true)
+    private String correoElectronico;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Vehiculo> listaVehiculos;
+
+}
