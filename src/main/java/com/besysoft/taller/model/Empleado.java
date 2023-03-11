e437e1af13389c1f6d5026b09ae44fe174bd3f01
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 20:33:57 -0300

    Update Empleado.java

--- src/main/java/com/besysoft/taller/model/Empleado.java
+++ src/main/java/com/besysoft/taller/model/Empleado.java
@@ -1,6 +1,10 @@
 package com.besysoft.taller.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
@@ -8,12 +12,17 @@ import java.io.Serializable;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Table(name="empleados")
 @DiscriminatorColumn(name="tipo_empleado")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class Empleado implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombres;
     private String apellido;
+    @Column(length = 15)
     private String celular;
     private String localidad;
     private String codigoPostal;
