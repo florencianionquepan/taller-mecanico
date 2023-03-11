0b9fbb43080306befb665cdf791e546bfe3c8e0a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 18:46:38 -0300

    Create Empleado.java

---
+++ src/main/java/com/besysoft/taller/model/Empleado.java
@@ -0,0 +1,23 @@
+package com.besysoft.taller.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Table(name="empleados")
+public abstract class Empleado implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombres;
+    private String apellido;
+    private String celular;
+    private String localidad;
+    private String codigoPostal;
+    private String calle;
+    private String numero;
+    private String departamento;
+    private String piso;
+}
