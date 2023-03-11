9ed96e44d2480cdf5a0b51b9d05f1b8e900a2eb6
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 20:34:08 -0300

    Update Vehiculo.java

--- src/main/java/com/besysoft/taller/model/Vehiculo.java
+++ src/main/java/com/besysoft/taller/model/Vehiculo.java
@@ -1,13 +1,30 @@
 package com.besysoft.taller.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name="vehiculos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vehiculo implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false,unique = true)
     private String patente;
     private int anio;
     private String color;
     private String marca;
     private String modelo;
 
+    @ManyToOne
+    private Cliente cliente;
 }
