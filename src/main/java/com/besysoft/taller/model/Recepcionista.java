9c11e0959d68da9f993163b660f57bcf6b77a544
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 20:34:19 -0300

    Update Recepcionista.java

--- src/main/java/com/besysoft/taller/model/Recepcionista.java
+++ src/main/java/com/besysoft/taller/model/Recepcionista.java
@@ -2,11 +2,19 @@ package com.besysoft.taller.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @Entity
 @DiscriminatorValue(value="Recep")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Recepcionista extends Empleado implements Serializable {
 
 }
