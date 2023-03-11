c4dc17c70d3c7c70e7ddea83535b71c73fbccc46
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-11 20:32:45 -0300

    Update Administrativo.java

--- src/main/java/com/besysoft/taller/model/Administrativo.java
+++ src/main/java/com/besysoft/taller/model/Administrativo.java
@@ -2,11 +2,20 @@ package com.besysoft.taller.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @Entity
 @DiscriminatorValue("Admin")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Administrativo extends Empleado implements Serializable {
 
 }
