4d98f8d8609ca05d7683e093d881904b0d8a1091
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-12 18:02:27 -0300

    Update ClienteServiceImple.java

--- src/main/java/com/besysoft/taller/service/implementation/ClienteServiceImple.java
+++ src/main/java/com/besysoft/taller/service/implementation/ClienteServiceImple.java
@@ -1,8 +1,10 @@
 package com.besysoft.taller.service.implementation;
 
 import com.besysoft.taller.model.Cliente;
+import com.besysoft.taller.model.Vehiculo;
 import com.besysoft.taller.repository.ClienteRepository;
 import com.besysoft.taller.service.interfaces.IClienteService;
+import com.besysoft.taller.service.interfaces.IVehiculoService;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,9 +13,11 @@ import java.util.Optional;
 public class ClienteServiceImple implements IClienteService {
 
     private final ClienteRepository repo;
+    private final IVehiculoService vehiService;
 
-    public ClienteServiceImple(ClienteRepository repo) {
+    public ClienteServiceImple(ClienteRepository repo, IVehiculoService vehiService) {
         this.repo = repo;
+        this.vehiService = vehiService;
     }
 
     @Override
@@ -35,7 +39,34 @@ public class ClienteServiceImple implements IClienteService {
     }
 
     @Override
-    public Cliente modiCliente(Cliente cliente) {
-        return null;
+    public Cliente recibeCliente(Cliente cliente) {
+        //con dto viene un auto pero la entidad tiene una lista
+        Vehiculo vehiculo= cliente.getListaVehiculos().get(0);
+        if(!this.vehiService.existeVehiculo(vehiculo)){
+            if(!this.existeCliente(cliente)){
+                throw new RuntimeException(
+                        String.format("El vehiculo con patente %s no existe," +
+                                vehiculo.getPatente(),
+                                "Y el cliente con email %s no se encuentra registrado," +
+                                cliente.getCorreoElectronico() +
+                                "Debe crear ambos y volver a intentarlo")
+                );
+            }else{
+                throw new RuntimeException(
+                        String.format("El vehiculo con patente %s no existe," +
+                                "por favor cree uno nuevo y vuelva a intentarlo", vehiculo.getPatente())
+                );
+            }
+        }
+        if(!this.existeCliente(cliente)){
+            throw new RuntimeException(
+                    String.format("El cliente con email %s no se encuentra registrado," +
+                            cliente.getCorreoElectronico() +
+                            "Debe crearlo y volver a intentarlo")
+            );
+        }
+        //is existe cliente y vehiculo, vincularlos:
+        //this.addVehiculo()
+        return this.repo.save(cliente);
     }
 }
