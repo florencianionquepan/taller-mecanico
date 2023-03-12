479b10986f33f53b7d4886b61df3c99809f2342e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-12 20:19:17 -0300

    Update IClienteService.java

--- src/main/java/com/besysoft/taller/service/interfaces/IClienteService.java
+++ src/main/java/com/besysoft/taller/service/interfaces/IClienteService.java
@@ -6,5 +6,5 @@ public interface IClienteService {
 
     Cliente altaCliente(Cliente cliente);
     boolean existeCliente(String email);
-    Cliente recibeCliente(Cliente cliente);
+    Cliente recibeCliente(String correo, String patente);
 }
