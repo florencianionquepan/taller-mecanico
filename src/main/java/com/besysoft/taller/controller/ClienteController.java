9abf4fc3a3c18ff91f73608b3f4ab896d88d0efa
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-12 20:18:47 -0300

    Update ClienteController.java

--- src/main/java/com/besysoft/taller/controller/ClienteController.java
+++ src/main/java/com/besysoft/taller/controller/ClienteController.java
@@ -1,29 +1,42 @@
 package com.besysoft.taller.controller;
 
+import com.besysoft.taller.dto.ClienteRecibidoDTO;
+import com.besysoft.taller.dto.mapper.IClienteMapper;
 import com.besysoft.taller.model.Cliente;
 import com.besysoft.taller.service.interfaces.IClienteService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/vehiculos_clientes")
+@RequestMapping("/clientes")
 public class ClienteController {
 
     private final IClienteService service;
+    private final IClienteMapper mapper;
 
-    public ClienteController(IClienteService service) {
+    public ClienteController(IClienteService service, IClienteMapper mapper) {
         this.service = service;
+        this.mapper = mapper;
     }
 
-    @GetMapping("/{cliente}")
-    public ResponseEntity<?> recibirClienteVehiculo(@RequestParam Cliente cliente){
-        //conversion del dto a entidad
-        //llamamos a modificar cliente del servicio que vaya arrojando los exceptions a medida que
-        //va verificando
-        this.service.recibeCliente(cliente);
-        return null;
+    public Map<String,Object> mensajeBody= new HashMap<>();
+
+    private ResponseEntity<?> successResponse(Cliente cliente){
+        mensajeBody.put("Success",Boolean.TRUE);
+        mensajeBody.put("data",cliente);
+        return ResponseEntity.ok(mensajeBody);
+    }
+
+    @GetMapping("/vehiculo")
+    public ResponseEntity<?> recibirVehiculoCliente(@RequestParam String email, String patente){
+        Cliente ent=this.service.recibeCliente(email, patente);
+        return this.successResponse(ent);
     }
 }
