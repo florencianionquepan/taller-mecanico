923781d2d37f6362911a85f52b9d0a3a2e246b02
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-12 13:46:31 -0300

    Update Empleado.java

--- src/main/java/com/besysoft/taller/model/Empleado.java
+++ src/main/java/com/besysoft/taller/model/Empleado.java
@@ -24,10 +24,6 @@ public abstract class Empleado implements Serializable {
     private String apellido;
     @Column(length = 15)
     private String celular;
-    private String localidad;
-    private String codigoPostal;
-    private String calle;
-    private String numero;
-    private String departamento;
-    private String piso;
+    @Embedded
+    private Direccion direccion;
 }
