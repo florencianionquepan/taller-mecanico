66949188cabec23eca7d4c0a95ffea3290702838
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-12 13:00:51 -0300

    Create IClienteService.java

---
+++ src/main/java/com/besysoft/taller/service/interfaces/IClienteService.java
@@ -0,0 +1,9 @@
+package com.besysoft.taller.service.interfaces;
+
+import com.besysoft.taller.model.Cliente;
+
+public interface IClienteService {
+
+    Cliente altaCliente(Cliente cliente);
+    boolean existeCliente(Cliente cliente);
+}
