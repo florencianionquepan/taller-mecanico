d89a0a02eb1ddadddc8c109973acaf51aa49f3dd
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-13 15:22:38 -0300

    Create ClienteDTO.java

---
+++ src/main/java/com/besysoft/taller/dto/ClienteDTO.java
@@ -0,0 +1,30 @@
+package com.besysoft.taller.dto;
+
+import com.besysoft.taller.model.Vehiculo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Embedded;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ClienteDTO {
+    private Long id;
+
+    @Embedded
+    private PersonaDTO personaDTO;
+
+    private String tel;
+    @NotEmpty
+    @NotNull
+    private String email;
+
+    @JsonIgnoreProperties(value="clientes")
+    private List<VehiculoDTO> vehiculos;
+}
