d0ae67417184fc9fec2604f8a89514f7a39cff4f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-13 18:00:34 -0300

    Update VehiculoDTO.java

--- src/main/java/com/besysoft/taller/dto/VehiculoDTO.java
+++ src/main/java/com/besysoft/taller/dto/VehiculoDTO.java
@@ -3,9 +3,17 @@ package com.besysoft.taller.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class VehiculoDTO {
 
     private Long id;
