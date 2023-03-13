3d29cc38c99f93c3d511262a2b5fed3ab0cf9a16
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-13 18:47:22 -0300

    Update IVehiculoService.java

--- src/main/java/com/besysoft/taller/service/interfaces/IVehiculoService.java
+++ src/main/java/com/besysoft/taller/service/interfaces/IVehiculoService.java
@@ -1,9 +1,12 @@
 package com.besysoft.taller.service.interfaces;
 
+import com.besysoft.taller.model.Cliente;
 import com.besysoft.taller.model.Vehiculo;
 
 public interface IVehiculoService {
 
     Vehiculo altaVehiculo(Vehiculo vehiculo);
     boolean existeVehiculo(Vehiculo vehiculo);
+    public Vehiculo buscarPorPatente(String patente);
+    void addClienteVehiculo(Vehiculo vehiculo, Cliente cliente);
 }
