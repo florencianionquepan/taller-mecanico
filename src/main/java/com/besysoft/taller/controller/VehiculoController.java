ab204ff5a2fca3f7b2e50278db7bfb960bf37de2
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-13 18:05:10 -0300

    Add altaVehiculo

--- src/main/java/com/besysoft/taller/controller/VehiculoController.java
+++ src/main/java/com/besysoft/taller/controller/VehiculoController.java
@@ -1,7 +1,11 @@
 package com.besysoft.taller.controller;
 
+import com.besysoft.taller.dto.VehiculoDTO;
+import com.besysoft.taller.dto.mapper.IVehiculoMapper;
 import com.besysoft.taller.model.Vehiculo;
 import com.besysoft.taller.service.interfaces.IVehiculoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,13 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class VehiculoController {
 
     private final IVehiculoService service;
+    private final IVehiculoMapper mapper;
 
-    public VehiculoController(IVehiculoService service) {
+    public VehiculoController(IVehiculoService service, IVehiculoMapper mapper) {
         this.service = service;
+        this.mapper = mapper;
     }
 
     @PostMapping
-    public ResponseEntity<?> altaVehiculo(@RequestBody Vehiculo vehi){
-        return null;
+    public ResponseEntity<?> altaVehiculo(@RequestBody @Valid VehiculoDTO dto){
+        Vehiculo veNuevo=this.mapper.mapToEntity(dto);
+        Vehiculo veGu=this.service.altaVehiculo(veNuevo);
+        VehiculoDTO veResp=this.mapper.mapToDto(veGu);
+        return ResponseEntity.status(HttpStatus.CREATED).body(veResp);
     }
 }
