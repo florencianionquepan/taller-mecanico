c2b65b9db3798a864d17841022851fc93163639e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-14 16:53:13 -0300

    Update VehiculoController.java
    Add buscarVehiculoPorPatente in order to create ordenTrabajo with Vehiculo

--- src/main/java/com/besysoft/taller/controller/VehiculoController.java
+++ src/main/java/com/besysoft/taller/controller/VehiculoController.java
@@ -7,10 +7,10 @@ import com.besysoft.taller.service.interfaces.IVehiculoService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/vehiculos")
@@ -24,11 +24,22 @@ public class VehiculoController {
         this.mapper = mapper;
     }
 
+    public Map<String,Object> mensajeBody= new HashMap<>();
+
     @PostMapping
     public ResponseEntity<?> altaVehiculo(@RequestBody @Valid VehiculoDTO dto){
         Vehiculo veNuevo=this.mapper.mapToEntity(dto);
         Vehiculo veGu=this.service.altaVehiculo(veNuevo);
         VehiculoDTO veResp=this.mapper.mapToDto(veGu);
         return ResponseEntity.status(HttpStatus.CREATED).body(veResp);
+    }
+
+    @GetMapping("/{patente}")
+    public ResponseEntity<?> buscarVehiculoPorPatente(@PathVariable String patente){
+        Vehiculo vehPatente=this.service.buscarPorPatente(patente);
+        VehiculoDTO resp=this.mapper.mapToDto(vehPatente);
+        mensajeBody.put("Success",Boolean.TRUE);
+        mensajeBody.put("data",resp);
+        return ResponseEntity.ok(mensajeBody);
     }
 }
