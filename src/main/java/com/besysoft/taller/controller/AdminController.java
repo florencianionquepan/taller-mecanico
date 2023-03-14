4f3edfc9df8627e72060839070ba0ee136802515
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-14 15:55:19 -0300

    Create AdminController.java

---
+++ src/main/java/com/besysoft/taller/controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.besysoft.taller.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/administradores")
+public class AdminController {
+
+}
