daae14d22aa684d9bfb5c4d9467fee5c695c99a1
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-14 18:25:38 -0300

    Update Vehiculo.java

--- src/main/java/com/besysoft/taller/model/Vehiculo.java
+++ src/main/java/com/besysoft/taller/model/Vehiculo.java
@@ -2,10 +2,8 @@ package com.besysoft.taller.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.List;
@@ -16,6 +14,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Vehiculo implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
