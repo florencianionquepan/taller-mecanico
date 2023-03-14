13e1e89fb39698fe85e390ba3dad24f39e4434ef
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-14 15:55:09 -0300

    Create IAdminService.java

---
+++ src/main/java/com/besysoft/taller/service/interfaces/IAdminService.java
@@ -0,0 +1,7 @@
+package com.besysoft.taller.service.interfaces;
+
+import com.besysoft.taller.model.Administrativo;
+
+public interface IAdminService {
+    Administrativo altaAdmin(Administrativo admin);
+}
