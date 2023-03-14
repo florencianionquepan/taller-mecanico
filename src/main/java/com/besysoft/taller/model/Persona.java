4f542101ef6ef6103d90033d3061adcf36d5848f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-14 19:34:37 -0300

    Update Persona.java

--- src/main/java/com/besysoft/taller/model/Persona.java
+++ src/main/java/com/besysoft/taller/model/Persona.java
@@ -1,16 +1,15 @@
 package com.besysoft.taller.model;
 
 import javax.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 
 @Embeddable
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Persona {
 
     @Column(length = 100, nullable = false)
