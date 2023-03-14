e52a231f5da8ffdabbc3583d05c6f7f0887fb8e6
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-14 13:09:55 -0300

    Create OrdenTrabajoRepository.java

---
+++ src/main/java/com/besysoft/taller/repository/OrdenTrabajoRepository.java
@@ -0,0 +1,7 @@
+package com.besysoft.taller.repository;
+
+import com.besysoft.taller.model.OrdenTrabajo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrdenTrabajoRepository extends CrudRepository<OrdenTrabajo, Long> {
+}
