d8bb49a317456690603510423208f43bf9392380
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-15 01:08:00 -0300

    Update IAdminService.java

--- src/main/java/com/besysoft/taller/service/interfaces/IAdminService.java
+++ src/main/java/com/besysoft/taller/service/interfaces/IAdminService.java
@@ -2,6 +2,10 @@ package com.besysoft.taller.service.interfaces;
 
 import com.besysoft.taller.model.Administrativo;
 
+import java.util.List;
+
 public interface IAdminService {
     Administrativo altaAdmin(Administrativo admin);
+
+    List<Administrativo> verAdmin();
 }
