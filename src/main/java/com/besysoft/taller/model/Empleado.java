6bfdb03ee4259c887b41906c4dccb9acb1338c73
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-15 18:38:21 -0300

    Update Empleado.java

--- src/main/java/com/besysoft/taller/model/Empleado.java
+++ src/main/java/com/besysoft/taller/model/Empleado.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -23,4 +24,16 @@ public abstract class Empleado implements Serializable {
     @Embedded
     private Persona persona;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Empleado)) return false;
+        Empleado empleado = (Empleado) o;
+        return Objects.equals(getId(), empleado.getId()) && Objects.equals(getPersona(), empleado.getPersona());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPersona());
+    }
 }
