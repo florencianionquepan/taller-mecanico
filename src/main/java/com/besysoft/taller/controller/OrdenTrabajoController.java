5dd1622d5e6749a2e367bfeb13f3d68062de1446
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-15 01:23:43 -0300

    Update OrdenTrabajoController.java

--- src/main/java/com/besysoft/taller/controller/OrdenTrabajoController.java
+++ src/main/java/com/besysoft/taller/controller/OrdenTrabajoController.java
@@ -1,3 +1,32 @@
+package com.besysoft.taller.controller;
+
+import com.besysoft.taller.dto.OrdenTrabajoDTO;
+import com.besysoft.taller.dto.mapper.IOrdenTrabajoMapper;
+import com.besysoft.taller.service.interfaces.IOrdenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+
+@RestController
+@RequestMapping("/ordenes")
+public class OrdenTrabajoController {
+
+    private final IOrdenService service;
+    private final IOrdenTrabajoMapper mapper;
+    private Logger logger= LoggerFactory.getLogger(OrdenTrabajoController.class);
+
+    public OrdenTrabajoController(IOrdenService service, IOrdenTrabajoMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
     @PostMapping
     public ResponseEntity<?> altaOrden(@RequestBody OrdenTrabajoDTO orden){
         Long datetime = System.currentTimeMillis();
