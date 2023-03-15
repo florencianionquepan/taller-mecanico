0a78ed6edbbc765fdec8284eacdf7261339fd31e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-15 19:45:47 -0300

    Create MecanicoRepository.java

---
+++ src/main/java/com/besysoft/taller/repository/MecanicoRepository.java
@@ -0,0 +1,7 @@
+package com.besysoft.taller.repository;
+
+import com.besysoft.taller.model.Mecanico;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MecanicoRepository extends CrudRepository<Mecanico,Long> {
+}
