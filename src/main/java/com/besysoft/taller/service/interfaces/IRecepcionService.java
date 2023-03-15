eed20b3a392f7a9c3875c1979204ab37b0e51334
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-15 15:54:41 -0300

    Create IRecepcionService.java

---
+++ src/main/java/com/besysoft/taller/service/interfaces/IRecepcionService.java
@@ -0,0 +1,10 @@
+package com.besysoft.taller.service.interfaces;
+
+import com.besysoft.taller.model.Recepcionista;
+
+import java.util.List;
+
+public interface IRecepcionService {
+    Recepcionista altaRecepcion(Recepcionista recep);
+    List<Recepcionista> verTodas();
+}
