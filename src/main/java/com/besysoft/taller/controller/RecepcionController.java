e08b28c6b85fcb47d561bd8b630948d15883670a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-15 17:35:46 -0300

    Update RecepcionController.java

--- src/main/java/com/besysoft/taller/controller/RecepcionController.java
+++ src/main/java/com/besysoft/taller/controller/RecepcionController.java
@@ -5,13 +5,13 @@ import com.besysoft.taller.dto.RecepcionistaDTO;
 import com.besysoft.taller.dto.mapper.IRecepMapper;
 import com.besysoft.taller.model.Recepcionista;
 import com.besysoft.taller.service.interfaces.IRecepcionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +23,7 @@ public class RecepcionController {
     private final IRecepcionService service;
     private final IRecepMapper mapper;
     public Map<String,Object> mensajeBody= new HashMap<>();
+    private Logger logger= LoggerFactory.getLogger(RecepcionController.class);
 
     public RecepcionController(IRecepcionService service, IRecepMapper mapper) {
         this.service = service;
@@ -30,7 +31,7 @@ public class RecepcionController {
     }
 
     @PostMapping
-    public ResponseEntity<?> altaRecep(RecepcionistaDTO dto){
+    public ResponseEntity<?> altaRecep(@RequestBody @Valid RecepcionistaDTO dto){
         Recepcionista nueva=this.service.altaRecepcion(this.mapper.mapToEntity(dto));
         return ResponseEntity.status(HttpStatus.CREATED).body(this.mapper.mapToDto(nueva));
     }
