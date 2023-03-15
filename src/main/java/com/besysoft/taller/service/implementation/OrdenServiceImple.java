423fd20b2f0dc7cd0e64841d05a3a82476ebfbce
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-15 01:42:06 -0300

    Update OrdenServiceImple.java

--- src/main/java/com/besysoft/taller/service/implementation/OrdenServiceImple.java
+++ src/main/java/com/besysoft/taller/service/implementation/OrdenServiceImple.java
@@ -3,7 +3,11 @@ package com.besysoft.taller.service.implementation;
 import com.besysoft.taller.model.OrdenTrabajo;
 import com.besysoft.taller.repository.OrdenTrabajoRepository;
 import com.besysoft.taller.service.interfaces.IOrdenService;
+import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+
+@Service
 public class OrdenServiceImple implements IOrdenService {
 
     private final OrdenTrabajoRepository repo;
@@ -14,7 +18,10 @@ public class OrdenServiceImple implements IOrdenService {
 
     @Override
     public OrdenTrabajo altaOrden(OrdenTrabajo orden) {
-        //orden.setFechaIngreso();
+        orden.setEstado("CREADA");
+        Long datetime = System.currentTimeMillis();
+        Timestamp fechaIn = new Timestamp(datetime);
+        orden.setFechaIngreso(fechaIn);
         return this.repo.save(orden);
     }
 
