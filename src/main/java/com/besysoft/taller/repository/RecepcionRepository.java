c940a1873b03698f1a571a7cb18d151c3526eb52
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-15 15:54:30 -0300

    Create RecepcionRepository.java

---
+++ src/main/java/com/besysoft/taller/repository/RecepcionRepository.java
@@ -0,0 +1,7 @@
+package com.besysoft.taller.repository;
+
+import com.besysoft.taller.model.Recepcionista;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecepcionRepository extends CrudRepository<Recepcionista,Long> {
+}
