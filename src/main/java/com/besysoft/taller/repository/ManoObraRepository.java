2fb8a4d8469be8bd63e4151e0bd647c9fdadd3bb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-16 01:46:41 -0300

    Create ManoObraRepository.java

---
+++ src/main/java/com/besysoft/taller/repository/ManoObraRepository.java
@@ -0,0 +1,7 @@
+package com.besysoft.taller.repository;
+
+import com.besysoft.taller.model.ManoObra;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ManoObraRepository extends CrudRepository<ManoObra,Long> {
+}
