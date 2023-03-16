c7682c1488eb15047c730c14447dc948e1fd2e26
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-16 17:46:11 -0300

    Update IMecanicoService.java

--- src/main/java/com/besysoft/taller/service/interfaces/IMecanicoService.java
+++ src/main/java/com/besysoft/taller/service/interfaces/IMecanicoService.java
@@ -1,5 +1,6 @@
 package com.besysoft.taller.service.interfaces;
 
+import com.besysoft.taller.model.ManoObra;
 import com.besysoft.taller.model.Mecanico;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +11,6 @@ public interface IMecanicoService {
 
     Mecanico altaMecanico(Mecanico mecanico);
     List<Mecanico> verTodos();
+    Mecanico buscarById(Long id);
+    void addManoObra(Mecanico mecanico, ManoObra manoObra);
 }
