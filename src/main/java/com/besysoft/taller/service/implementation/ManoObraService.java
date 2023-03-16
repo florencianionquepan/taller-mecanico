cbe5955f0e7e8e3d772623d11c4a8623fe3e6d82
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-16 17:47:45 -0300

    Update NewManoObra

--- src/main/java/com/besysoft/taller/service/implementation/ManoObraService.java
+++ src/main/java/com/besysoft/taller/service/implementation/ManoObraService.java
@@ -1,22 +1,38 @@
 package com.besysoft.taller.service.implementation;
 
 import com.besysoft.taller.model.ManoObra;
+import com.besysoft.taller.model.Mecanico;
+import com.besysoft.taller.model.OrdenTrabajo;
 import com.besysoft.taller.repository.ManoObraRepository;
 import com.besysoft.taller.service.interfaces.IManoObraService;
+import com.besysoft.taller.service.interfaces.IMecanicoService;
+import com.besysoft.taller.service.interfaces.IOrdenService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ManoObraService implements IManoObraService {
 
     private final ManoObraRepository repo;
+    private final IMecanicoService mecaService;
+    private final IOrdenService ordenService;
 
-    public ManoObraService(ManoObraRepository repo) {
+    public ManoObraService(ManoObraRepository repo, IMecanicoService mecaService, IOrdenService ordenService) {
         this.repo = repo;
+        this.mecaService = mecaService;
+        this.ordenService = ordenService;
     }
 
     @Override
     public ManoObra altaManoObra(ManoObra manoObra) {
-        return this.repo.save(manoObra);
+        //para traer el objeto entero busco por id a orden de trabajo y a mecanico
+        //esto para que response de mano obra me muestre los atributos de estos objetos
+        //guardarle al mecanico esta mano de obra
+        Mecanico meca=this.mecaService.buscarById(manoObra.getMecanico().getId());
+        this.mecaService.addManoObra(meca,manoObra);
+        OrdenTrabajo orden=this.ordenService.buscarById(manoObra.getOrdenTrabajo().getId());
+        //ordenTrabajo no posee mano_obra. relacion 1 a 1
+        ManoObra guardada=this.repo.save(manoObra);
+        return guardada;
     }
 
     @Override
