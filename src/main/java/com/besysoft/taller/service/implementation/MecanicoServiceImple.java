f3e5240f97712502ff7aa1d5971a2503feb43373
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-16 17:46:18 -0300

    Update MecanicoServiceImple.java

--- src/main/java/com/besysoft/taller/service/implementation/MecanicoServiceImple.java
+++ src/main/java/com/besysoft/taller/service/implementation/MecanicoServiceImple.java
@@ -1,11 +1,14 @@
 package com.besysoft.taller.service.implementation;
 
+import com.besysoft.taller.exception.NonExistingException;
+import com.besysoft.taller.model.ManoObra;
 import com.besysoft.taller.model.Mecanico;
 import com.besysoft.taller.repository.MecanicoRepository;
 import com.besysoft.taller.service.interfaces.IMecanicoService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MecanicoServiceImple implements IMecanicoService {
@@ -24,5 +27,24 @@ public class MecanicoServiceImple implements IMecanicoService {
     @Override
     public List<Mecanico> verTodos() {
         return (List<Mecanico>) this.repo.findAll();
+    }
+
+    @Override
+    public Mecanico buscarById(Long id) {
+        Optional<Mecanico> oMe=this.repo.findById(id);
+        if(oMe.isEmpty()){
+            throw new NonExistingException(
+                    String.format("El mecanico con id %d no existe ",
+                            id)
+            );
+        }
+        return oMe.get();
+    }
+
+    @Override
+    public void addManoObra(Mecanico mecanico, ManoObra manoObra) {
+        List<ManoObra> obras=mecanico.getListaManoObra();
+        obras.add(manoObra);
+        mecanico.setListaManoObra(obras);
     }
 }
