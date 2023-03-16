ce3ea84eed733f601697574a39b3db82246e304e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-16 19:01:33 -0300

    Update Mecanico.java

--- src/main/java/com/besysoft/taller/model/Mecanico.java
+++ src/main/java/com/besysoft/taller/model/Mecanico.java
@@ -3,10 +3,7 @@ package com.besysoft.taller.model;
 import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.List;
@@ -17,6 +14,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Mecanico implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
