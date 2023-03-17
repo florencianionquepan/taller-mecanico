253da322076090d5508787bcf755c155a3467050
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 14:07:16 -0300

    Update ManoObraController.java

--- src/main/java/com/besysoft/taller/controller/ManoObraController.java
+++ src/main/java/com/besysoft/taller/controller/ManoObraController.java
@@ -4,10 +4,7 @@ import com.besysoft.taller.model.ManoObra;
 import com.besysoft.taller.service.interfaces.IManoObraService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/manoObras")
@@ -20,9 +17,10 @@ public class ManoObraController {
         this.service = service;
     }
 
-    @PostMapping
-    public ResponseEntity<?> nuevaManoObra(@RequestBody  ManoObra manoObra){
-        ManoObra nueva=this.service.altaManoObra(manoObra);
-        return ResponseEntity.status(HttpStatus.CREATED).body(nueva);
+    @PutMapping("/{id}")
+    public ResponseEntity<?> modiManoObra(@PathVariable Long id,
+                                           @RequestBody ManoObra manoObra){
+        ManoObra actual=this.service.modiManoObra(manoObra,id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(actual);
     }
 }
