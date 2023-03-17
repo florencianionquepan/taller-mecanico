268bb696c4991e43ac3d162dcc601be8f75c0527
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-16 21:54:03 -0300

    Update MecanicoServiceImple.java
    Add function to obtain Mecanico with lower mano obras

--- src/main/java/com/besysoft/taller/service/implementation/MecanicoServiceImple.java
+++ src/main/java/com/besysoft/taller/service/implementation/MecanicoServiceImple.java
@@ -1,19 +1,26 @@
 package com.besysoft.taller.service.implementation;
 
+import com.besysoft.taller.controller.OrdenTrabajoController;
 import com.besysoft.taller.exception.NonExistingException;
+import com.besysoft.taller.model.EstadoOrden;
 import com.besysoft.taller.model.ManoObra;
 import com.besysoft.taller.model.Mecanico;
 import com.besysoft.taller.repository.MecanicoRepository;
 import com.besysoft.taller.service.interfaces.IMecanicoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MecanicoServiceImple implements IMecanicoService {
 
     private final MecanicoRepository repo;
+    private Logger logger= LoggerFactory.getLogger(MecanicoServiceImple.class);
 
     public MecanicoServiceImple(MecanicoRepository repo) {
         this.repo = repo;
@@ -46,5 +53,30 @@ public class MecanicoServiceImple implements IMecanicoService {
         List<ManoObra> obras=mecanico.getListaManoObra();
         obras.add(manoObra);
         mecanico.setListaManoObra(obras);
+    }
+
+
+    //El metodo me retorna el mecanico activo con menor cantidad de mano de obra
+    @Override
+    public Mecanico mecanicoConMenosObras(){
+        List<Mecanico> activos=((List<Mecanico>) this.repo.findAll()).stream()
+                        .filter(meca->meca.getActivo().toString().equals("T"))
+                        .collect(Collectors.toList());
+        Mecanico mecaMenosObras=activos.stream()
+                .min(Comparator.comparingInt(mecanico ->
+                        cantidadObrasVigentes(mecanico.getListaManoObra()))
+                ).orElse(null);
+        logger.info("Mecanico con menos obras"+mecaMenosObras);
+        return mecaMenosObras;
+    }
+
+    private Integer cantidadObrasVigentes(List<ManoObra> manoObras){
+        Integer manoObrasCreadas= (int) manoObras.stream()
+                .filter(obra -> obra.getOrdenTrabajo().getEstado().equals(EstadoOrden.CREADA))
+                .count();
+        Integer manosObrasReparacion= (int) manoObras.stream()
+                .filter(obra -> obra.getOrdenTrabajo().getEstado().equals(EstadoOrden.REPARACION))
+                .count();
+        return manoObrasCreadas+manosObrasReparacion;
     }
 }
