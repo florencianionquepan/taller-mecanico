e3321bfe63c1e68f5ed0c7eb55804b5564d6182e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 19:43:26 -0300

    Create IRepuestoService.java

---
+++ src/main/java/com/besysoft/taller/service/interfaces/IRepuestoService.java
@@ -0,0 +1,10 @@
+package com.besysoft.taller.service.interfaces;
+
+import com.besysoft.taller.model.Repuesto;
+
+import java.util.List;
+
+public interface IRepuestoService {
+    Repuesto altaRepuesto(Repuesto repuesto);
+    List<Repuesto> verRepuestos();
+}
