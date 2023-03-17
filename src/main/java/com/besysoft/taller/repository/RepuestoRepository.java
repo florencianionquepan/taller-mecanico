8f06151a9c4dbe6a8761d83d5ea90d3caae4c599
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 19:43:23 -0300

    Create RepuestoRepository.java

---
+++ src/main/java/com/besysoft/taller/repository/RepuestoRepository.java
@@ -0,0 +1,8 @@
+package com.besysoft.taller.repository;
+
+
+import com.besysoft.taller.model.Repuesto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepuestoRepository extends CrudRepository<Repuesto,Long> {
+}
