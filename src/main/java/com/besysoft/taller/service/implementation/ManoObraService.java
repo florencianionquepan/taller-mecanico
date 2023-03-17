b1714fa91d341715a4dd039661634e6f3cbeeb34
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 14:05:57 -0300

    ManoObra has OrdenService

--- src/main/java/com/besysoft/taller/service/implementation/ManoObraService.java
+++ src/main/java/com/besysoft/taller/service/implementation/ManoObraService.java
@@ -14,27 +14,20 @@ public class ManoObraService implements IManoObraService {
 
     private final ManoObraRepository repo;
     private final IMecanicoService mecaService;
-    private final IOrdenService ordenService;
 
-    public ManoObraService(ManoObraRepository repo, IMecanicoService mecaService, IOrdenService ordenService) {
+    public ManoObraService(ManoObraRepository repo, IMecanicoService mecaService) {
         this.repo = repo;
         this.mecaService = mecaService;
-        this.ordenService = ordenService;
     }
 
     @Override
     public ManoObra altaManoObra(ManoObra manoObra) {
-        //para traer el objeto entero busco por id a orden de trabajo
-        //esto para que response de mano obra me muestre los atributos de la orden
-        //guardarle al mecanico esta mano de obra
         //el mecanico se selecciona de forma automatica
-        //Mecanico meca=this.mecaService.buscarById(manoObra.getMecanico().getId());
         Mecanico meca=this.mecaService.mecanicoConMenosObras();
+        //guardarle al mecanico esta mano de obra
         this.mecaService.addManoObra(meca,manoObra);
-        OrdenTrabajo orden=this.ordenService.buscarById(manoObra.getOrdenTrabajo().getId());
         //ordenTrabajo no posee mano_obra. relacion 1 a 1
         manoObra.setMecanico(meca);
-        manoObra.setOrdenTrabajo(orden);
         ManoObra guardada=this.repo.save(manoObra);
         return guardada;
     }
