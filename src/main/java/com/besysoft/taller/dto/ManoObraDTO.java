a236c40f30d6bdd438012182618d7853276f1b44
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-18 17:45:21 -0300

    Create ManoObraDTO.java

---
+++ src/main/java/com/besysoft/taller/dto/ManoObraDTO.java
@@ -0,0 +1,30 @@
+package com.besysoft.taller.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+//este DTO lo utiliza OrdenTrabajoDTO
+public class ManoObraDTO {
+    @NotNull
+    @Size(min=1)
+    private Long id;
+    @NotNull
+    @NotEmpty
+    private String detalle;
+    @NotNull
+    @Temporal(TemporalType.TIME)
+    private Date duracionHs;
+}
