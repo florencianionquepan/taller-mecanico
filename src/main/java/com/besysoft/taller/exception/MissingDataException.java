a71c840016027f5a0eb9fecf4bafcaf1910e43d3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 22:43:29 -0300

    Create MissingDataException.java

---
+++ src/main/java/com/besysoft/taller/exception/MissingDataException.java
@@ -0,0 +1,8 @@
+package com.besysoft.taller.exception;
+
+public class MissingDataException extends RuntimeException{
+
+    public MissingDataException(String message) {
+        super(message);
+    }
+}
