63bbfd99c6981c5bcd61304f2ad781b02ffcf589
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 23:51:47 -0300

    Update IOrdenService.java

--- src/main/java/com/besysoft/taller/service/interfaces/IOrdenService.java
+++ src/main/java/com/besysoft/taller/service/interfaces/IOrdenService.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface IOrdenService {
     OrdenTrabajo altaOrden(OrdenTrabajo orden);
     OrdenTrabajo iniciarReparacion(Long id);
+    OrdenTrabajo finalizarReparacion(Long id, OrdenTrabajo orden);
     List<OrdenTrabajo> verTodas();
     OrdenTrabajo buscarById(Long id);
     void addManoObra(OrdenTrabajo orden, ManoObra obra);
