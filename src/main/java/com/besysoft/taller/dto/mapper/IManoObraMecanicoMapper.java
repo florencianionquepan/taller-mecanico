4dfa112104823740043c7135a45ce803e7fad71d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-18 17:45:35 -0300

    Update IManoObraMecanicoMapper.java

--- src/main/java/com/besysoft/taller/dto/mapper/IManoObraMecanicoMapper.java
+++ src/main/java/com/besysoft/taller/dto/mapper/IManoObraMecanicoMapper.java
@@ -1,13 +1,15 @@
 package com.besysoft.taller.dto.mapper;
 
-import com.besysoft.taller.dto.ManoObraMecanicoDTO;
+import com.besysoft.taller.dto.ManoObraDTO;
 import com.besysoft.taller.model.ManoObra;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface IManoObraMecanicoMapper {
-    ManoObra mapToEntity(ManoObraMecanicoDTO dto);
-    ManoObraMecanicoDTO mapToDto(ManoObra entidad);
-    List<ManoObra> mapListToEntities(List<ManoObraMecanicoDTO> dtos);
-    List<ManoObraMecanicoDTO> mapListToDto(List<ManoObra> entities);
+    ManoObra mapToEntity(ManoObraDTO dto);
+    ManoObraDTO mapToDto(ManoObra entidad);
+    List<ManoObra> mapListToEntities(List<ManoObraDTO> dtos);
+    List<ManoObraDTO> mapListToDto(List<ManoObra> entities);
 }
