c282fb2f36010d0cf6d61a4fe1b77b8614f81a7e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 22:43:42 -0300

    Create DetalleOrdenRepository.java

---
+++ src/main/java/com/besysoft/taller/repository/DetalleOrdenRepository.java
@@ -0,0 +1,7 @@
+package com.besysoft.taller.repository;
+
+import com.besysoft.taller.model.DetalleOrdenTrabajo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DetalleOrdenRepository extends CrudRepository<DetalleOrdenTrabajo,Long> {
+}
