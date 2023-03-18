c9907416cf456fae36bd572a0b9dcf174f9254ed
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 22:50:27 -0300

    Update RepuestoServiceImple.java

--- src/main/java/com/besysoft/taller/service/implementation/RepuestoServiceImple.java
+++ src/main/java/com/besysoft/taller/service/implementation/RepuestoServiceImple.java
@@ -1,11 +1,14 @@
 package com.besysoft.taller.service.implementation;
 
+import com.besysoft.taller.exception.NonExistingException;
 import com.besysoft.taller.model.Repuesto;
+import com.besysoft.taller.model.Vehiculo;
 import com.besysoft.taller.repository.RepuestoRepository;
 import com.besysoft.taller.service.interfaces.IRepuestoService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RepuestoServiceImple implements IRepuestoService {
@@ -24,5 +27,16 @@ public class RepuestoServiceImple implements IRepuestoService {
     @Override
     public List<Repuesto> verRepuestos() {
         return (List<Repuesto>) this.repo.findAll();
+    }
+
+    @Override
+    public Repuesto buscarById(Long id) {
+        Optional<Repuesto> oVe=this.repo.findById(id);
+        if(oVe.isEmpty()){
+            throw new NonExistingException(
+                   String.format("El vehiculo con id %d no existe", id)
+            );
+        }
+        return oVe.get();
     }
 }
