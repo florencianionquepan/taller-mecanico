cf0a4e695dfcb1a4a3352992f0f2a6fc5de09d8b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 22:43:46 -0300

    Create IDetalleOrdenService.java

---
+++ src/main/java/com/besysoft/taller/service/interfaces/IDetalleOrdenService.java
@@ -0,0 +1,9 @@
+package com.besysoft.taller.service.interfaces;
+
+import com.besysoft.taller.model.DetalleOrdenTrabajo;
+
+import java.util.List;
+
+public interface IDetalleOrdenService {
+    DetalleOrdenTrabajo altaDetalleOrden(DetalleOrdenTrabajo detalle);
+}
