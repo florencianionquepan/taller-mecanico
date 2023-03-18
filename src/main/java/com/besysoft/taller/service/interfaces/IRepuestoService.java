ec2f0777a47e1bfc07561e7703de81cacea05b1a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 22:50:21 -0300

    Add buscarRepuestoById

--- src/main/java/com/besysoft/taller/service/interfaces/IRepuestoService.java
+++ src/main/java/com/besysoft/taller/service/interfaces/IRepuestoService.java
@@ -1,10 +1,12 @@
 package com.besysoft.taller.service.interfaces;
 
 import com.besysoft.taller.model.Repuesto;
+import com.besysoft.taller.model.Vehiculo;
 
 import java.util.List;
 
 public interface IRepuestoService {
     Repuesto altaRepuesto(Repuesto repuesto);
     List<Repuesto> verRepuestos();
+    Repuesto buscarById(Long id);
 }
