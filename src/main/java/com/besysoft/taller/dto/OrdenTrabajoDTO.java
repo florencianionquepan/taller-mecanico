800f3938cd3e8fad78e4b319c70abeade9b3de01
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 22:44:16 -0300

    Update OrdenTrabajoDTO.java

--- src/main/java/com/besysoft/taller/dto/OrdenTrabajoDTO.java
+++ src/main/java/com/besysoft/taller/dto/OrdenTrabajoDTO.java
@@ -1,7 +1,10 @@
 package com.besysoft.taller.dto;
 
+import com.besysoft.taller.model.DetalleOrdenTrabajo;
 import com.besysoft.taller.model.EstadoOrden;
+import com.besysoft.taller.model.ManoObra;
 import com.besysoft.taller.model.Recepcionista;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.Temporal;
@@ -13,6 +16,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -49,8 +53,13 @@ public class OrdenTrabajoDTO {
     //se solicitara cuando se haga la factura
     private AdministrativoDTO administrativo;
 
-    ///voy a tener un get buscarPorPatente en controller de vehiculo en el lado del front
-    //si o si debe ser de un vehiculo
     @NotNull
+    @Valid
     private VehiculoDTO vehiculo;
+
+    @JsonIgnoreProperties("value=ordenTrabajo")
+    private List<ManoObraRespDTO> listaManoObra;
+
+    private List<DetalleOrdenTrabajo> detalleOrdenes;
+
 }
