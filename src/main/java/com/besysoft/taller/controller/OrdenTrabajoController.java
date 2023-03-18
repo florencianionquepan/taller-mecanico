428078be6e9792f93b7961af282eb392a918400c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-18 18:40:50 -0300

    Update OrdenTrabajoController.java

--- src/main/java/com/besysoft/taller/controller/OrdenTrabajoController.java
+++ src/main/java/com/besysoft/taller/controller/OrdenTrabajoController.java
@@ -1,7 +1,9 @@
 package com.besysoft.taller.controller;
 
+import com.besysoft.taller.dto.OrdenNuevaDTO;
 import com.besysoft.taller.dto.OrdenTrabajoDTO;
-import com.besysoft.taller.dto.mapper.IOrdenTrabajoMapper;
+import com.besysoft.taller.dto.mapper.IOrdenNuevaMapper;
+import com.besysoft.taller.dto.mapper.OrdenNuevaMapper;
 import com.besysoft.taller.model.OrdenTrabajo;
 import com.besysoft.taller.service.interfaces.IOrdenService;
 import org.slf4j.Logger;
@@ -20,21 +22,21 @@ import java.util.Map;
 public class OrdenTrabajoController {
 
     private final IOrdenService service;
-    private final IOrdenTrabajoMapper mapper;
+    private final IOrdenNuevaMapper nuevaMapper;
     private Logger logger= LoggerFactory.getLogger(OrdenTrabajoController.class);
 
-    public OrdenTrabajoController(IOrdenService service, IOrdenTrabajoMapper mapper) {
+    public OrdenTrabajoController(IOrdenService service, IOrdenNuevaMapper nuevaMapper) {
         this.service = service;
-        this.mapper = mapper;
+        this.nuevaMapper = nuevaMapper;
     }
 
     public Map<String,Object> mensajeBody= new HashMap<>();
 
     @PostMapping
     //Se genera la orden de trabajo
-    public ResponseEntity<?> altaOrden(@RequestBody @Valid OrdenTrabajoDTO orden){
-        OrdenTrabajo nueva=this.service.altaOrden(this.mapper.mapToEntity(orden));
-        OrdenTrabajoDTO nuevaResp=this.mapper.mapToDto(nueva);
+    public ResponseEntity<?> altaOrden(@RequestBody @Valid OrdenNuevaDTO orden){
+        OrdenTrabajo nueva=this.service.altaOrden(this.nuevaMapper.mapToEntity(orden));
+        OrdenNuevaDTO nuevaResp=this.nuevaMapper.mapToDto(nueva);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevaResp);
     }
 
@@ -61,7 +63,7 @@ public class OrdenTrabajoController {
 
     @GetMapping
     public ResponseEntity<?> verTodas(){
-        List<OrdenTrabajoDTO> ordenes=this.mapper.mapListToDto(this.service.verTodas());
+        List<OrdenNuevaDTO> ordenes=this.nuevaMapper.mapListToDto(this.service.verTodas());
         mensajeBody.put("Success",Boolean.TRUE);
         mensajeBody.put("data",ordenes);
         return ResponseEntity.ok(mensajeBody);
