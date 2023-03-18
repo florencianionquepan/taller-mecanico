280ea4b5961563f4da4087f8dd30e1ea8a4ae8b7
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-17 23:25:06 -0300

    Update DetalleOrdenTrabajo.java

--- src/main/java/com/besysoft/taller/model/DetalleOrdenTrabajo.java
+++ src/main/java/com/besysoft/taller/model/DetalleOrdenTrabajo.java
@@ -2,10 +2,8 @@ package com.besysoft.taller.model;
 
 import javax.persistence.*;
 import javax.validation.constraints.Digits;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -16,6 +14,7 @@ import java.math.BigDecimal;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class DetalleOrdenTrabajo implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
