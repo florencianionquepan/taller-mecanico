8f25a882b321332a992e1bf8587749249b5827cf
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-18 16:54:54 -0300

    Update IDetalleOrdenMapper.java

--- src/main/java/com/besysoft/taller/dto/mapper/IDetalleOrdenMapper.java
+++ src/main/java/com/besysoft/taller/dto/mapper/IDetalleOrdenMapper.java
@@ -4,7 +4,12 @@ import com.besysoft.taller.dto.DetalleOrdenDTO;
 import com.besysoft.taller.model.DetalleOrdenTrabajo;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 public interface IDetalleOrdenMapper {
     DetalleOrdenTrabajo mapToEntity(DetalleOrdenDTO dto);
     DetalleOrdenDTO mapToDto(DetalleOrdenTrabajo entidad);
+    List<DetalleOrdenTrabajo> mapListToEntities(List<DetalleOrdenDTO> dtos);
+    List<DetalleOrdenDTO> MapListToDto(List<DetalleOrdenTrabajo> entidades);
+
 }
