dd1c860f61e98f9f2ae83c47c9e63f1a30445609
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-19 14:57:03 -0300

    Create OrdenFacturadaDTO.java

---
+++ src/main/java/com/besysoft/taller/dto/OrdenFacturadaDTO.java
@@ -0,0 +1,37 @@
+package com.besysoft.taller.dto;
+
+import com.besysoft.taller.model.Administrativo;
+import com.besysoft.taller.model.EstadoOrden;
+import com.besysoft.taller.model.Vehiculo;
+import lombok.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class OrdenFacturadaDTO {
+    private Long id;
+    private EstadoOrden estado;
+
+    private LocalDateTime fechaPago;
+    private String formaPago;
+    private Integer cantidadCuotas;
+    private String tipoTarjeta;
+
+    @Digits(integer=17,fraction = 2)
+    private BigDecimal importeTotal;
+
+    @NotNull
+    @Valid
+    private Administrativo administrativo;
+    private Vehiculo vehiculo;
+}
