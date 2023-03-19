313e032b2484db556a0484382958f5fafbaf057e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-19 15:27:03 -0300

    Update AdminServiceImple.java

--- src/main/java/com/besysoft/taller/service/implementation/AdminServiceImple.java
+++ src/main/java/com/besysoft/taller/service/implementation/AdminServiceImple.java
@@ -1,11 +1,13 @@
 package com.besysoft.taller.service.implementation;
 
+import com.besysoft.taller.exception.NonExistingException;
 import com.besysoft.taller.model.Administrativo;
 import com.besysoft.taller.repository.AdminRepository;
 import com.besysoft.taller.service.interfaces.IAdminService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminServiceImple implements IAdminService {
@@ -24,5 +26,17 @@ public class AdminServiceImple implements IAdminService {
     @Override
     public List<Administrativo> verAdmin() {
         return (List<Administrativo>) this.repo.findAll();
+    }
+
+    @Override
+    public Administrativo buscarById(Long id) {
+        Optional<Administrativo> oAdmin=this.repo.findById(id);
+        if(oAdmin.isEmpty()){
+            throw new NonExistingException(
+                    String.format("El administrativo con id %d no existe",
+                            id)
+            );
+        }
+        return oAdmin.get();
     }
 }
