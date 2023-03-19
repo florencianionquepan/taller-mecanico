eb72e8926b4f68fd7b31d74117136d0ac8acd2e1
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-19 13:07:20 -0300

    Update ManoObraOrdenDTO.java

--- src/main/java/com/besysoft/taller/dto/ManoObraOrdenDTO.java
+++ src/main/java/com/besysoft/taller/dto/ManoObraOrdenDTO.java
@@ -1,10 +1,14 @@
 package com.besysoft.taller.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.time.LocalTime;
 import java.util.Date;
 
 @Getter
@@ -14,11 +18,14 @@ import java.util.Date;
 @ToString
 //DTO de mano de obra sin mecanico asignado.Su mapper sera inyectado por MecanicoMapper
 public class ManoObraOrdenDTO {
+    @NonNull
     private Long id;
     private String detalle;
-    @Temporal(TemporalType.TIME)
-    private Date duracionHs;
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime duracionHs;
 
+    @NotNull
+    @Valid
     @JsonIgnoreProperties(value={"recepcionista","administrativo"})
     private OrdenTrabajoDTO ordenTrabajo;
 }
