e0e8efe7e0c083a60a192516ba87219f21decac2
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-20 11:39:47 -0300

    Create MissingStateException.java

---
+++ src/main/java/com/besysoft/taller/exception/MissingStateException.java
@@ -0,0 +1,7 @@
+package com.besysoft.taller.exception;
+
+public class MissingStateException extends RuntimeException{
+    public MissingStateException(String message) {
+        super(message);
+    }
+}
