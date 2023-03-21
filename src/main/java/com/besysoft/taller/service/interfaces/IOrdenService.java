33f0100c534ed019e9893ba170c4ce3431553e02
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-21 10:15:50 -0300

    Update IOrdenService.java

--- src/main/java/com/besysoft/taller/service/interfaces/IOrdenService.java
+++ src/main/java/com/besysoft/taller/service/interfaces/IOrdenService.java
@@ -1,12 +1,14 @@
 package com.besysoft.taller.service.interfaces;
 
 import com.besysoft.taller.model.ManoObra;
+import com.besysoft.taller.model.Mecanico;
 import com.besysoft.taller.model.OrdenTrabajo;
 
 import java.util.List;
 
 public interface IOrdenService {
     OrdenTrabajo altaOrden(OrdenTrabajo orden);
+    OrdenTrabajo altaManoObra(Long id, Mecanico asignado);
     OrdenTrabajo iniciarReparacion(Long id);
     OrdenTrabajo finalizarReparacion(Long id, OrdenTrabajo orden);
     OrdenTrabajo facturarOrden(Long id, OrdenTrabajo orden);
