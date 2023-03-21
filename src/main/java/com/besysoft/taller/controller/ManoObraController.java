2d5023b3a62616eeebc087f61a9128fe1ecf153b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-21 10:40:25 -0300

    Update ManoObraController.java

--- src/main/java/com/besysoft/taller/controller/ManoObraController.java
+++ src/main/java/com/besysoft/taller/controller/ManoObraController.java
@@ -1,14 +1,9 @@
 package com.besysoft.taller.controller;
 
-import com.besysoft.taller.dto.ManoObraMecanicoOrdenDTO;
-import com.besysoft.taller.dto.mapper.IManoObraMecanicoOrdenMapper;
-import com.besysoft.taller.model.ManoObra;
 import com.besysoft.taller.service.interfaces.IManoObraService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,20 +12,11 @@ import java.util.Map;
 public class ManoObraController {
 
     private final IManoObraService service;
-    private final IManoObraMecanicoOrdenMapper mapper;
 
-    public ManoObraController(IManoObraService service, IManoObraMecanicoOrdenMapper mapper) {
+    public ManoObraController(IManoObraService service) {
         this.service = service;
-        this.mapper = mapper;
     }
 
     public Map<String,Object> mensajeBody= new HashMap<>();
 
-    @PostMapping
-    //El mecanico crea la mano de obra asignada a la orden de trabajo
-    public ResponseEntity<?> creacionManoObra(@RequestBody @Valid ManoObraMecanicoOrdenDTO dto){
-        ManoObra creada=this.service.altaManoObra(this.mapper.mapToEntity(dto));
-        ManoObraMecanicoOrdenDTO creadaResp=this.mapper.mapToDto(creada);
-        return ResponseEntity.status(HttpStatus.CREATED).body(creadaResp);
-    }
 }
