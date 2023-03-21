63f8778b5d0623eeb818da60f03e467303ceff14
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-21 09:10:47 -0300

    Update RecepcionServiceImple.java

--- src/main/java/com/besysoft/taller/service/implementation/RecepcionServiceImple.java
+++ src/main/java/com/besysoft/taller/service/implementation/RecepcionServiceImple.java
@@ -1,11 +1,13 @@
 package com.besysoft.taller.service.implementation;
 
+import com.besysoft.taller.exception.NonExistingException;
 import com.besysoft.taller.model.Recepcionista;
 import com.besysoft.taller.repository.RecepcionRepository;
 import com.besysoft.taller.service.interfaces.IRecepcionService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RecepcionServiceImple implements IRecepcionService {
@@ -32,7 +34,16 @@ public class RecepcionServiceImple implements IRecepcionService {
     }
 
     @Override
-    public boolean esCorrecta(Recepcionista recep){
-        return recep.equals(this.repo.findById(recep.getId()).get());
+    public Recepcionista buscarById(Long id) {
+        Optional<Recepcionista> oRecep=this.repo.findById(id);
+        if(oRecep.isEmpty()){
+            throw new NonExistingException(
+                    String.format("La recepcionista con id %d no existe",
+                            id
+                    )
+            );
+        }
+        return oRecep.get();
     }
+
 }
