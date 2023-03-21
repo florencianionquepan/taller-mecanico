1564d8a9c738546105067872a10d82eeaaca639d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-21 14:29:31 -0300

    Create DatosDummy for testing

---
+++ src/test/java/com/besysoft/taller/datos/DatosDummy.java
@@ -0,0 +1,22 @@
+package com.besysoft.taller.datos;
+
+import com.besysoft.taller.model.Cliente;
+import com.besysoft.taller.model.Persona;
+import com.besysoft.taller.model.Vehiculo;
+
+import java.util.ArrayList;
+
+public class DatosDummy {
+    public static Cliente getClienteIgor(){
+        return new Cliente(null,
+                new Persona("Igor","Gatito","462643","Bahia Blanca","8500","calle","300",null,null),
+                "462643","igor@yahoo.com",new ArrayList<Vehiculo>());
+    }
+
+    public static Cliente getClienteEmma(){
+        return new Cliente(null,
+                new Persona("Emma","Gatita","462014","Bahia Blanca","8500","calle","300",null,null),
+                "462014","emma@yahoo.com",new ArrayList<Vehiculo>());
+    }
+
+}
