dc166655b3d49e38978ff65e97ed779b03718638
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-21 20:23:03 -0300

    Create RepuestoServiceImpleTest.java

---
+++ src/test/java/com/besysoft/taller/service/implementation/RepuestoServiceImpleTest.java
@@ -0,0 +1,20 @@
+package com.besysoft.taller.service.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepuestoServiceImpleTest {
+
+    @Test
+    void altaRepuesto() {
+    }
+
+    @Test
+    void verRepuestos() {
+    }
+
+    @Test
+    void buscarById() {
+    }
+}
