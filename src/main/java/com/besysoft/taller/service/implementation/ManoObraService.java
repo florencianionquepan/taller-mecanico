d4fd8cc50989c7ee57486e5adb322b00ef088af0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-21 10:15:29 -0300

    Update ManoObraService.java

--- src/main/java/com/besysoft/taller/service/implementation/ManoObraService.java
+++ src/main/java/com/besysoft/taller/service/implementation/ManoObraService.java
@@ -15,23 +15,15 @@ public class ManoObraService implements IManoObraService {
 
     private final ManoObraRepository repo;
     private final IMecanicoService mecaService;
-    private final IOrdenService ordenService;
 
-    public ManoObraService(ManoObraRepository repo, IMecanicoService mecaService, IOrdenService ordenService) {
+    public ManoObraService(ManoObraRepository repo, IMecanicoService mecaService) {
         this.repo = repo;
         this.mecaService = mecaService;
-        this.ordenService = ordenService;
     }
 
     @Override
+    //manoObra viene con el mecanico asignado y la orden
     public ManoObra altaManoObra(ManoObra manoObra) {
-        //al buscarse se chequean que existan
-        Mecanico meca=this.mecaService.buscarById(manoObra.getMecanico().getId());
-        OrdenTrabajo orden=this.ordenService.buscarById(manoObra.getOrdenTrabajo().getId());
-        //agregarle al mecanico y a orden esta mano de obra
-        this.mecaService.addManoObra(meca,manoObra);
-        this.ordenService.addManoObra(orden,manoObra);
-        //manoObra.setMecanico(meca);
         return this.repo.save(manoObra);
     }
 
