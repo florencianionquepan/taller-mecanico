a84253612a5fd5fc17028b21d66c693193a491e7
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-22 14:15:42 -0300

    Create ManoObraServiceTest.java

---
+++ src/test/java/com/besysoft/taller/service/implementation/ManoObraServiceTest.java
@@ -0,0 +1,16 @@
+package com.besysoft.taller.service.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManoObraServiceTest {
+
+    @Test
+    void altaManoObra() {
+    }
+
+    @Test
+    void modiManoObra() {
+    }
+}
