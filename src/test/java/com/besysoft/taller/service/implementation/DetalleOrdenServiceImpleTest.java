1a48187e7dc66aa3a2043f6f20780a003eb80eca
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-22 16:50:23 -0300

    Update DetalleOrdenServiceImpleTest.java

--- src/test/java/com/besysoft/taller/service/implementation/DetalleOrdenServiceImpleTest.java
+++ src/test/java/com/besysoft/taller/service/implementation/DetalleOrdenServiceImpleTest.java
@@ -1,10 +1,16 @@
 package com.besysoft.taller.service.implementation;
 
+import com.besysoft.taller.datos.DatosDummy;
+import com.besysoft.taller.model.Administrativo;
+import com.besysoft.taller.model.DetalleOrdenTrabajo;
+import com.besysoft.taller.model.Repuesto;
 import com.besysoft.taller.repository.DetalleOrdenRepository;
 import com.besysoft.taller.service.interfaces.IRepuestoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class DetalleOrdenServiceImpleTest {
@@ -22,5 +28,20 @@ class DetalleOrdenServiceImpleTest {
 
     @Test
     void altaDetalleOrden() {
+        DetalleOrdenTrabajo detalle= DatosDummy.getDetalle();
+        Repuesto repu=detalle.getRepuesto();
+        when(repuService.buscarById(repu.getId()))
+                .thenReturn(repu);
+        when(repo.save(detalle))
+                .thenReturn(detalle);
+
+        service.altaDetalleOrden(detalle);
+
+        ArgumentCaptor<DetalleOrdenTrabajo> detalleArgumentCaptor=ArgumentCaptor
+                .forClass(DetalleOrdenTrabajo.class);
+        verify(repo).save(detalleArgumentCaptor.capture());
+
+        DetalleOrdenTrabajo detalleCaptor=detalleArgumentCaptor.getValue();
+        assertThat(detalleCaptor).isEqualTo(detalle);
     }
 }
