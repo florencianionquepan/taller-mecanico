4130b78fb4fd6064a7e09256e754f98ec18234d3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-22 16:50:43 -0300

    Create OrdenServiceImpleTest.java

---
+++ src/test/java/com/besysoft/taller/service/implementation/OrdenServiceImpleTest.java
@@ -0,0 +1,49 @@
+package com.besysoft.taller.service.implementation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdenServiceImpleTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void altaOrden() {
+    }
+
+    @Test
+    void altaManoObra() {
+    }
+
+    @Test
+    void iniciarReparacion() {
+    }
+
+    @Test
+    void finalizarReparacion() {
+    }
+
+    @Test
+    void facturarOrden() {
+    }
+
+    @Test
+    void cerrarOrden() {
+    }
+
+    @Test
+    void verTodas() {
+    }
+
+    @Test
+    void buscarById() {
+    }
+
+    @Test
+    void verByVehiculo() {
+    }
+}
