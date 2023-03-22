cee9b10cb7d22b9a8634c23081c521e369fce48f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-22 13:35:04 -0300

    Update MecanicoServiceImpleTest.java

--- src/test/java/com/besysoft/taller/service/implementation/MecanicoServiceImpleTest.java
+++ src/test/java/com/besysoft/taller/service/implementation/MecanicoServiceImpleTest.java
@@ -4,6 +4,7 @@ import com.besysoft.taller.datos.DatosDummy;
 import com.besysoft.taller.model.Administrativo;
 import com.besysoft.taller.model.ManoObra;
 import com.besysoft.taller.model.Mecanico;
+import com.besysoft.taller.model.OrdenTrabajo;
 import com.besysoft.taller.repository.AdminRepository;
 import com.besysoft.taller.repository.MecanicoRepository;
 import com.besysoft.taller.service.interfaces.IMecanicoService;
@@ -12,12 +13,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class MecanicoServiceImpleTest {
@@ -85,7 +86,7 @@ class MecanicoServiceImpleTest {
     void addManoObra() {
         //GIVEN
         Mecanico meca=DatosDummy.getMeca();
-        ManoObra obra=DatosDummy.getMO();
+        ManoObra obra=DatosDummy.getMOActiva();
         //WHEN
         service.addManoObra(meca,obra);
         //THEN
@@ -94,5 +95,26 @@ class MecanicoServiceImpleTest {
 
     @Test
     void mecanicoConMenosObras() {
+        //GIVEN
+        Mecanico menosActivo=DatosDummy.getMeca();
+        List<ManoObra> cerradas=new ArrayList<ManoObra>(
+                List.of(DatosDummy.getMOcerrada())
+        );
+        menosActivo.setListaManoObra(cerradas);
+
+        Mecanico masActivo=DatosDummy.getMecaActivo();
+        List<ManoObra> activas=new ArrayList<ManoObra>(
+                List.of(DatosDummy.getMOActiva())
+        );
+        masActivo.setListaManoObra(activas);
+
+        List<Mecanico> activos=new ArrayList<Mecanico>(
+                Arrays.asList(menosActivo,masActivo)
+        );
+        when(repo.findAll())
+                .thenReturn(activos);
+        //WHEN
+        //THEN
+        assertThat(service.mecanicoConMenosObras()).isEqualTo(menosActivo);
     }
 }
