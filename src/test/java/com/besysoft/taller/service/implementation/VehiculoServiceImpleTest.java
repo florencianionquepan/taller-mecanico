7580636e9d739442c14644f7078ca0afb4f69098
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-22 21:21:11 -0300

    Update VehiculoServiceImpleTest.java

--- src/test/java/com/besysoft/taller/service/implementation/VehiculoServiceImpleTest.java
+++ src/test/java/com/besysoft/taller/service/implementation/VehiculoServiceImpleTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
