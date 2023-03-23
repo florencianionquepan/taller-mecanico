ca4ffcbfc6774e87a545d99fc03a4d64d320dd81
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-23 13:59:29 -0300

    Update RepuestoController.java

--- src/main/java/com/besysoft/taller/controller/RepuestoController.java
+++ src/main/java/com/besysoft/taller/controller/RepuestoController.java
@@ -5,6 +5,7 @@ import com.besysoft.taller.dto.mapper.IRepuestoMapper;
 import com.besysoft.taller.model.Repuesto;
 import com.besysoft.taller.service.interfaces.IRepuestoService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +31,7 @@ public class RepuestoController {
     public Map<String,Object> mensajeBody= new HashMap<>();
 
     @GetMapping
+    @ApiOperation(value="Permite listar todos los repuestos existentes.")
     public ResponseEntity<?> verTodos(){
         List<RepuestoDTO> repuestos=this.mapper.mapListToDto(this.service.verRepuestos());
         mensajeBody.put("Success",Boolean.TRUE);
@@ -38,6 +40,7 @@ public class RepuestoController {
     }
 
     @PostMapping
+    @ApiOperation(value="Permite crear un nuevo repuesto.")
     public ResponseEntity<?> altaRepuesto(@RequestBody @Valid RepuestoDTO dto){
         Repuesto repuesto=this.mapper.mapToEntity(dto);
         Repuesto nuevo=this.service.altaRepuesto(repuesto);
